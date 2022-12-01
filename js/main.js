c21f2fcd7a60bd39308148cb9c671422b648720e
Levou07 <andishox04@gmail.com>
2022-12-01 21:48:42 +0500

    new commit

--- js/main.js
+++ js/main.js
@@ -1,70 +1,101 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((res)=> res.json())
+.then(data => mapper(data))
+let a = 0
+function mapper(data){
+    console.log(data);
+    data.forEach(e => {
+        let center__ul = document.querySelector('.center__ul')
+        let newbutton = document.createElement('button')
+        let newLi = document.createElement('li')
+        newLi.setAttribute('class', 'newLi')
+        newLi.innerHTML = e.username
 
-let inputEnd = document.querySelector('.input__end')
-let inp1 = document.querySelector('.inp1')
-let wrapper = document.querySelector('.wrapper')
-let wrapper2 = document.querySelector('.wrapper2')
-let wrapp = document.querySelector('.wrapp')
+        let newLi2 = document.createElement('li')
+        newLi2.setAttribute('class', 'newLi2')
+        newLi2.innerHTML = e.name
 
-let count = inputEnd.textContent = 0
-let inpV = inp1.value.length
+        newbutton.setAttribute('class', 'newBtn')
+        newbutton.appendChild(newLi)
+        newbutton.appendChild(newLi2)
 
-inp1.addEventListener('keyup', (e)=>{
-    if(inpV + 1){
-        inputEnd.innerHTML = `
-        <span class="input-group-text input__end">${(count += 1)}</span>
-        `
-        if(inputEnd.textContent > 10){
-            inputEnd.style.backgroundColor = 'red'
-        }
-    }
-    if(e.keyCode == 8){
-        inputEnd.innerHTML = `
-        <span class="input-group-text input__end">${(count -= 1)}</span>
-        `
-    }
+        
+        newbutton.setAttribute('id', a+=1)
+
+        center__ul.appendChild(newbutton)
+    });
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((resp) => resp.json())
+.then((posts) => {
+    console.log(posts);
+    tgpost(posts)
 })
-window.addEventListener('keyup', e=>{
-    console.log(e.keyCode);
-})
 
+function tgpost(data){
+    let Btn = document.querySelectorAll('.newBtn')
+    let rigth__tg = document.querySelector('.rigth__ul')
+    
+    Btn.forEach(w=>{
+        let num = 0
+        num += 1
+        w.addEventListener('click', ()=>{
+            rigth__tg.innerHTML = ''
+            data.forEach(r=>{
+                if(w.id == r.userId){
+                    const newLi = document.createElement('li')
+                    newLi.innerHTML = r.title
+                    newLi.setAttribute('class', 'post')
+                    newLi.classList.add('tg__blue')
+                    rigth__tg.appendChild(newLi)
+                    const newLi2 = document.createElement('li')
+                    let plus1 = r.userId + 1
+                    if(plus1){
+                        newLi2.innerHTML = r.title
+                        newLi2.setAttribute('class', 'post')
+                        newLi2.style.marginLeft = '-30px'
+                        rigth__tg.appendChild(newLi2)
+                    }
+                }
+            })
+        })
+        
+        let myIcon = document.querySelectorAll('.icons')
+        let Btn = document.querySelectorAll('.newBtn')
+        myIcon.forEach((h)=>{
+            rigth__tg.innerHTML = ''
+                h.addEventListener('click', ()=>{
+                    rigth__tg.innerHTML = ''
 
-// if(inp1.value == 1){
-//     wrapp.innerHTML = `<div class="input-group has-validation">
-//     <span class="input-group-text">@</span>
-//     <div class="form-floating is-invalid">
-//       <input type="text" class="form-control is-invalid" id="floatingInputGroup2" placeholder="Username" required>
-//       <label for="floatingInputGroup2">Username</label>
-//     </div>
-//     <div class="invalid-feedback">
-//       Please choose a username.
-//     </div>
-//   </div>`
-// }
-
-// inp1.addEventListener('keyup', e=>{
-//     console.log(inp1.value);
-//     let inpL = inp1.value.length
-//     if(inpL >= 10){
-//         wrapp.innerHTML = `<div class="input-group has-validation">
-//         <span class="input-group-text">@</span>
-//         <div class="form-floating is-invalid">
-//           <input type="text" class="form-control is-invalid" id="floatingInputGroup2" placeholder="Username" required>
-//           <label for="floatingInputGroup2">Enrer your username</label>
-//         </div>
-//         <div class="invalid-feedback">
-//           Please choose a username.
-//         </div>
-//       </div>`
-//     }
-//     wrapp.innerHTML = ''
-//     if(inpL <= 9){
-//         wrapp.innerHTML = `
-//         <div class="input-group mb-3">
-//   <span class="input-group-text">@</span>
-//   <div class="form-floating">
-//     <input type="text" class="form-control" id="floatingInputGroup1" placeholder="Username">
-//     <label for="floatingInputGroup1">Username</label>
-//   </div>
-// </div>`
-//     }
-// })
+                    Btn.forEach(b => {
+                        if(h.id == 1){
+                            b.style.display = 'none'
+                            Btn[0].style.display = 'block'                            
+                            Btn[1].style.display = 'block'
+                        }else if(h.id == 2){
+                            b.style.display = 'none'
+                            Btn[2].style.display = 'block'                            
+                            Btn[3].style.display = 'block'
+                        }else if(h.id == 3){
+                            b.style.display = 'none'
+                            Btn[4].style.display = 'block'                            
+                            Btn[5].style.display = 'block'
+                        }
+                        else if(h.id == 4){
+                            b.style.display = 'none'
+                            Btn[6].style.display = 'block'                            
+                            Btn[7].style.display = 'block'
+                        }else if(h.id == 5){
+                            b.style.display = 'none'
+                            Btn[8].style.display = 'block'                            
+                            Btn[9].style.display = 'block'
+                        }else{
+                            console.log('error');
+                        }
+                    })
+                })
+                     
+        })
+    })
+}
