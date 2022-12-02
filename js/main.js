a44f67e9495066010703ff7894cbaa0064d7b784
Levou07 <andishox04@gmail.com>
2022-12-02 20:29:02 +0500

    new

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let rigth__tg = document.querySelector('.rigth__ul')
+
 fetch('https://jsonplaceholder.typicode.com/users')
 .then((res)=> res.json())
 .then(data => mapper(data))
@@ -63,18 +65,46 @@ function tgpost(data){
             })
         })
     })
+
+    let btn = document.querySelectorAll('.btn')
+    let newBttn = document.querySelectorAll('.newBtn')
+    
+    btn.forEach(e=>{
+        rigth__tg.innerHTML = ''
+        e.addEventListener('click', ()=>{
+            newBttn.forEach(a => {
+                rigth__tg.innerHTML = ''
+                if(e.id == 1){
+                    a.style.display = 'none'
+                    newBttn[0].style.display = 'block'
+                    newBttn[1].style.display = 'block'
+                    console.log(e.id);
+                }
+                else if(e.id == 2){
+                    a.style.display = 'none'
+                    newBttn[2].style.display = 'block'
+                    newBttn[3].style.display = 'block'
+                }
+                else if(e.id == 3){
+                    a.style.display = 'none'
+                    newBttn[4].style.display = 'block'
+                    newBttn[5].style.display = 'block'
+                }
+                else if(e.id == 4){
+                    a.style.display = 'none'
+                    newBttn[6].style.display = 'block'
+                    newBttn[7].style.display = 'block'
+                }
+                else if(e.id == 5){
+                    a.style.display = 'none'
+                    newBttn[8].style.display = 'block'
+                    newBttn[9].style.display = 'block'
+                }
+                else{
+                    console.log('hato');
+                }
+            })
+        })
+    })    
 }
-},100)
-
-let btn = document.querySelectorAll('.btn')
-
-btn.forEach(e=>{
-    e.addEventListener('click', (e)=>{
-        let newBtn = document.querySelectorAll('.newBtn')
-        if(e == e){
-            newBtn[0].classList.add('block')
-            newBtn[1].style.display = 'block'
-            newBtn.style.display = 'none'
-        }
-    })
-})
+},400)
